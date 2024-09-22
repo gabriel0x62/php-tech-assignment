dca324ea2863fdd58c4956a53101a3882b7143b7
Gabriel B <gabriel.bazdragan@gmail.com>
2024-09-22 21:38:23 +0000

    Implement simple webhook

--- public/index.php
+++ public/index.php
@@ -2,13 +2,29 @@
 
 declare(strict_types=1);
 
+use App\Enums\GatewayEnum;
+
 require_once __DIR__ . '/../vendor/autoload.php';
 
-use App\Enums\OutputTypeEnum;
-use App\Services\NotificationHandlerService;
+if (empty($_FILES)) {
+    header("HTTP/1.1 418 I'm a teapot");
+    exit("I'm a teapot");
+}
 
-$handler = new NotificationHandlerService(OutputTypeEnum::CSV);
-$handler->handle();
+foreach ($_FILES as $key => $file) {
+    $gateway = GatewayEnum::tryFrom($key);
+    
+    if ($gateway) {
+        $filePath = realpath("../payment-notifications/{$gateway->getFilename()}");
+        file_put_contents($filePath, file_get_contents($file['tmp_name']));
+    }
+}
 
-$handler = new NotificationHandlerService(OutputTypeEnum::TXT);
-$handler->handle();
+// use App\Enums\OutputTypeEnum;
+// use App\Services\NotificationHandlerService;
+
+// $handler = new NotificationHandlerService(OutputTypeEnum::CSV);
+// $handler->handle();
+
+// $handler = new NotificationHandlerService(OutputTypeEnum::TXT);
+// $handler->handle();
